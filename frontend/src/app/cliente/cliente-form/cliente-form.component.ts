6ffb8f3b4610a7b9b25aa47067c4b997a8d4598e
joaoaugustoma <joaoaugustoananias@gmail.com>
2022-06-22 21:42:58 -0300

    Update Cliente Form

--- frontend/src/app/cliente/cliente-form/cliente-form.component.ts
+++ frontend/src/app/cliente/cliente-form/cliente-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from "../shared/cliente";
+import {ClienteService} from "../shared/cliente.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-cliente-form',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente = new Cliente();
+  constructor(
+    private clienteService: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const cpf = this.route.snapshot.paramMap.get('cpf'); //pegar na rota atual o parametro
+    if (cpf) {
+      this.clienteService.getById(cpf)?.subscribe((cliente) => {
+          if (cliente) {
+            this.cliente = cliente;
+          }
+        }, erro => {
+          alert("Erro ao buscar o cliente com cpf: "+cpf);
+        }
+      )
+    };
+  }
+
+  public salvar(cliente: Cliente) {
+    let acao="criado";
+    if(cliente.cpf){
+      acao = "alterado";
+    }
+    this.clienteService.salvar(cliente)?.subscribe((cliente) => {
+      alert("Cliente "+acao+" com sucesso!")
+      console.log(cliente);
+      this.cliente = cliente;
+      this.router.navigate(['cliente']);
+
+    }, erro => {
+      alert(erro?.error?.message);
+      console.log(erro);
+    })
   }
 
 }
