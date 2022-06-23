92cf7cb873f65d0955af8690591ca740c2fad053
joaoaugustoma <joaoaugustoananias@gmail.com>
2022-06-22 21:43:10 -0300

    Update Cliente List

--- frontend/src/app/cliente/cliente-list/cliente-list.component.ts
+++ frontend/src/app/cliente/cliente-list/cliente-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from "../shared/cliente";
+import {ClienteService} from "../shared/cliente.service";
 
 @Component({
   selector: 'app-cliente-list',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteListComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[] = [];
+
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe((clientes: Cliente[]) => {
+      console.log("Cliente", clientes);
+      this.clientes = clientes;
+    });
+  }
+
+  confirmaRemocao(cliente: Cliente) {
+    let mensagem = "Deseja remover o cliente: " + cliente.primeiroNome + " " + cliente.sobreNome + ", Matricula: " + cliente.cpf + "?";
+    if (confirm(mensagem)) {
+      this.clienteService.remover(cliente.cpf)?.subscribe((cliente) => {
+        let clienteIdx = this.clientes.findIndex((value) => value.cpf == cliente.cpf);
+        this.clientes.splice(clienteIdx, 1);
+        alert("Cliente removido com sucesso!");
+
+      }, erro => {
+        alert("Erro ao excluir cliente. Mensagem: " +erro?.error?.message);
+      });
+    }
   }
 
 }
